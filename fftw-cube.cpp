fdec5fca69ae816bdf86c41595a6809cc9a2f6a4
Lagrang3 <eduardo.quintana@pm.me>
2022-02-10 10:02:08 +0100

    first approach to print into hdf5 files

--- fftw-cube.cpp
+++ fftw-cube.cpp
@@ -6,7 +6,9 @@
 #include <boost/mpi/communicator.hpp>
 namespace mpi = boost::mpi;
 #include <complex>
+#include <string>
 #include <fftw3-mpi.h>
+#include <hdf5.h>
 
 #include "cube_fill.hpp"
 
@@ -21,11 +23,20 @@ struct fftw_cube
     
     fftw_complex *my_data;
     
+    hid_t complex_id;
+    
     fftw_cube(std::size_t N, const MPI_Comm& raw_com):
         global_len{N},
         my_cube_fill(N),
         my_com(raw_com,mpi::comm_duplicate)
     {
+        {
+            fftw_complex tmp;
+            complex_id = H5Tcreate(H5T_COMPOUND,sizeof(tmp));
+            H5Tinsert(complex_id,"real",0,H5T_NATIVE_DOUBLE);
+            H5Tinsert(complex_id,"imag",sizeof(double),H5T_NATIVE_DOUBLE);
+        }
+    
         fftw_mpi_init();
         
         ptrdiff_t local_n0, local_0_start;
@@ -48,13 +59,55 @@ struct fftw_cube
             for(std::size_t j=0;j<global_len;++j)
             for(std::size_t k=0;k<global_len;++k)
             {
-                std::complex<double> val = my_cube_fill(local_start+i,j,k);
+                auto val = my_cube_fill(local_start+i,j,k);
                 
                 const std::size_t pos = k + global_len*( j + global_len*i);
                 my_data[pos][0] = val.real();
                 my_data[pos][1] = val.imag();
             }
         
+    }
+    
+    void save_to_file(std::string path)
+    {
+        hid_t plist_id = H5Pcreate(H5P_FILE_ACCESS);
+        MPI_Info info = MPI_INFO_NULL;
+        H5Pset_fapl_mpio(plist_id,my_com,info);
+        hid_t file_id = H5Fcreate(path.c_str(),H5F_ACC_TRUNC,H5P_DEFAULT,plist_id);
+        H5Pclose(plist_id);
+        
+        constexpr hsize_t DIM = 3;
+        hsize_t count[DIM],offset[DIM];
+        for(auto i=0U;i<DIM;++i)
+            count[i] = global_len;
+            
+        hid_t filespace = H5Screate_simple(DIM,count,NULL);
+        hid_t dset_id = H5Dcreate(file_id, "My Dataset", complex_id, filespace,
+            H5P_DEFAULT,H5P_DEFAULT,H5P_DEFAULT);
+        H5Sclose(filespace);
+        
+        count[0]=local_len;
+        count[1]=global_len;
+        count[2]=global_len;
+        offset[0]=local_start;
+        offset[1]=0;
+        offset[2]=0;
+        hid_t memspace = H5Screate_simple(DIM,count,NULL);
+        
+        filespace = H5Dget_space(dset_id);
+        H5Sselect_hyperslab(filespace,H5S_SELECT_SET,offset,NULL,count,NULL);
+        
+        plist_id = H5Pcreate(H5P_DATASET_XFER);
+        H5Pset_dxpl_mpio(plist_id,H5FD_MPIO_COLLECTIVE);
+        
+        herr_t status = H5Dwrite(dset_id, complex_id, memspace, filespace,
+            plist_id, my_data);
+        
+        H5Dclose(dset_id);
+        H5Sclose(filespace);
+        H5Sclose(memspace);
+        H5Pclose(plist_id);
+        H5Fclose(file_id);
     }
     
     void execute()
@@ -74,7 +127,9 @@ int main()
 {
     mpi::environment env;
     mpi::communicator world;
-    fftw_cube cube(128,world);
+    fftw_cube cube(8,world);
+    cube.save_to_file("fftw.h5");
     cube.execute();
+    cube.save_to_file("fftw-ft.h5");
     return 0;
 }
